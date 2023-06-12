2c46a5984078edb39aa7e488ba51658eb3e2022e
Austin Lake <53884490+austinlake04@users.noreply.github.com>
2023-06-12 10:18:54 -0400

    fixed environment setup

--- setup.sh
+++ setup.sh
@@ -5,7 +5,6 @@ if ! conda -V; then
     bash Miniforge3-$(uname)-$(uname -m).sh -b -p "${HOME}/conda"
     rm -rf Miniforge3-$(uname)-$(uname -m).sh
     source "${HOME}/conda/etc/profile.d/conda.sh"
-    conda activate
 fi
 
 conda env create -f env.yaml
@@ -13,11 +12,12 @@ conda env create -f env.yaml
 cwd=$PWD
 cd $(cd "$(dirname "$0")"; pwd)
 
-git submodule update --init --recursive
 source env.sh
 cd LEPHARE
 python setup.py install
 cd source
 make
+cd ../../pylephare
+python setup.py install
 
 cd ${cwd}
