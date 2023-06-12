d8be11e00910d078a4d7d0cde432c100a01039b4
Austin Lake <53884490+austinlake04@users.noreply.github.com>
2023-06-12 10:18:54 -0400

    fixed environment setup

--- env.sh
+++ env.sh
@@ -3,6 +3,8 @@
 conda activate roman_filters
 
 SCRIPT_DIR="$(cd "$(dirname "$0")"; pwd)"
-
+cd ${SCRIPT_DIR}
 export LEPHAREDIR=${SCRIPT_DIR}/LEPHARE
-export LEPHAREWORK=${SCRIPT_DIR}/lepharework
+export LEPHAREWORK=${SCRIPT_DIR}/py
+git submodule update --init --recursive
+cd -
