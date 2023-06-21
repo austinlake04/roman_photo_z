211c960fb205aab6f4b517e68f3876418d1a9446
Austin Lake <53884490+austinlake04@users.noreply.github.com>
2023-06-21 11:36:00 -0400

    BPZ config

--- env.sh
+++ env.sh
@@ -4,7 +4,10 @@ conda activate roman_filters
 
 SCRIPT_DIR="$(cd "$(dirname "$0")"; pwd)"
 cd ${SCRIPT_DIR}
+
 export LEPHAREDIR=${SCRIPT_DIR}/LEPHARE
 export LEPHAREWORK=${SCRIPT_DIR}/py
+export BPZDATAPATH=${SCRIPT_DIR}/DESC_BPZ/src/desc_bpz/data_files/
+
 git submodule update --init --recursive
 cd -
