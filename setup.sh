97ce1588c73bb210d3690e9ef0d18a9ebd89967c
Austin Lake <53884490+austinlake04@users.noreply.github.com>
2023-07-10 16:54:42 -0400

    fixed setup script

--- setup.sh
+++ setup.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 
 if ! conda -V; then
-    curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
-    bash Miniforge3-$(uname)-$(uname -m).sh -b -p "${HOME}/conda"
-    rm -rf Miniforge3-$(uname)-$(uname -m).sh
+    curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-$(uname)-$(uname -m).sh"
+    bash Mambaforge-$(uname)-$(uname -m).sh -b -p "${HOME}/conda"
+    rm -rf Mambaforge-$(uname)-$(uname -m).sh
     source "${HOME}/conda/etc/profile.d/conda.sh"
 fi
 
 conda env create -f env.yml
 
 cwd=$PWD
-cd $(cd "$(dirname "$0")"; pwd)
+cd ${BASH_SOURCE}
 
 source env.sh
-cd LEPHARE
-python setup.py install
-cd source
-make
-cd ../../pylephare
-python setup.py install
 
-cd ${cwd}
+cd $cwd
